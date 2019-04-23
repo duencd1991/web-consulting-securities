10a36d8af4e304ff9e6f9f5044077834ba505b87
Nguyen Canh Duc Due <Due.NguyenCanhDuc@thanglongsc.com.vn>
2019-04-23 18:09:18 +0700

    Doing training-service

--- src/views/services/tradingInstruction/tradingInstruction.js
+++ src/views/services/tradingInstruction/tradingInstruction.js
@@ -134,44 +134,87 @@ class TradingInstrucion extends Component {
             <div className='title'>HƯỚNG DẪN GIAO DỊCH</div>
             <div className='sub-title'>CHỨNG KHOÁN PHÁI SINH</div>
           </div>
-          <div className='guide-menu'>
-            <div className='title'>HƯỚNG DẪN GIAO DỊCH</div>
-            <hr />
-            <div className='list-menu-box'>
-              {
-                listGuide.map((item, index) => {
-                  return <ul key={index} className={selectedMenu === index? 'menu-item-box active' : 'menu-item-box'}>
-                    {
-                      selectedMenu === index ? <i className="fas fa-angle-up arrow" /> : <i className="fas fa-angle-down arrow" />
-                    }
-                    <li className={selectedMenu === index ? 'menu-catalog active' : 'menu-catalog' } onClick={()=>this.selectMenu(index)}>{ item.catalog}</li>
-                    {
-                      item.list.map((item, index) => {
-                        return <li key={index}>
-                          <i className="fas fa-angle-double-right"></i>
-                          {item.title}
-                        </li>
-                      })
-                    }
-                  </ul>
-                })
-              }
+          <div className='content-box'>
+            <div className='guide-menu'>
+              <div className='title'>HƯỚNG DẪN GIAO DỊCH</div>
+              <hr />
+              <div className='list-menu-box'>
+                {
+                  listGuide.map((item, index) => {
+                    return <ul key={index} className={selectedMenu === index? 'menu-item-box active' : 'menu-item-box'}>
+                      {
+                        selectedMenu === index ? <i className="fas fa-angle-up arrow" /> : <i className="fas fa-angle-down arrow" />
+                      }
+                      <li className={selectedMenu === index ? 'menu-catalog active' : 'menu-catalog' } onClick={()=>this.selectMenu(index)}>{ item.catalog}</li>
+                      {
+                        item.list.map((item, index) => {
+                          return <li key={index}>
+                            <i className="fas fa-angle-double-right"></i>
+                            {item.title}
+                          </li>
+                        })
+                      }
+                    </ul>
+                  })
+                }
+              </div>
             </div>
-          </div>
-          <div className='top-guide'>
-            <div className='title'>HƯỚNG DẪN ĐƯỢC XEM NHIỀU NHẤT</div>
-            <ul className='box-top-guide'>
-              {
-                listTopGuide.map((item, index) => {
-                  return <li key={index}>
-                    <i className="fas fa-angle-double-right"></i>
-                    {
-                      item.title
-                    }
-                  </li>
-                })
-              }
-            </ul>
+            <div className='top-guide'>
+              <div className='title'>HƯỚNG DẪN ĐƯỢC XEM NHIỀU NHẤT</div>
+              <ul className='box-top-guide'>
+                {
+                  listTopGuide.map((item, index) => {
+                    return <li key={index}>
+                      <i className="fas fa-angle-double-right"></i>
+                      {
+                        item.title
+                      }
+                    </li>
+                  })
+                }
+              </ul>
+              <div className='register-box'>
+                <p className='register-text'>Bạn có <span>TÀI KHOẢN</span></p>
+                <p className='register-text'>giao dịch <span>CHỨNG KHOÁN</span> chưa?</p>
+                <button className='btn btn-register'>
+                  <i className="far fa-user"></i>
+                  MỞ TÀI KHOẢN
+                </button>
+              </div>
+            </div>
+            <div className='contact-box'>
+              <div className='title'>LIÊN HỆ NHÂN VIÊN HỖ TRỢ</div>
+              <hr />
+              <div className='contact-content-box'>
+                <div className='info-box'>
+                  <div className='title'>PHÒNG DỊCH VỤ KHÁCH HÀNG ONLINE</div>
+                  <div class='info-content'>
+                    <i className="fas fa-map-marker-alt"></i>
+                    <span className='info-address'>Số 3 Liễu Giai, Quận Ba Đình, Hà Nội</span>
+                  </div>
+                  <div className='info-content'>
+                    <i className="fas fa-phone"></i>
+                    <span className='info-hotline'>1900 9088</span>
+                  </div>
+                  <div className='info-content'>
+                    <i className="fas fa-envelope"></i>
+                    <span className='info-email'>cskh@mbs.com.vn</span>
+                  </div>
+                </div>
+                <div className='msg-box'>
+                  <div className='title'>GỬI LỜI NHẮN</div>
+                  <input className='form-control msg-text' type='text' placeHolder='Họ và tên *'/>
+                  <input className='form-control msg-text' type='text' placeHolder='Điện thoại *'/>
+                  <input className='form-control msg-text' type='text' placeHolder='Email *'/>
+                  <textarea class="form-control msg-text" rows="3" placeHolder='Nội dung *'/>
+
+                  <button className='btn btn-send-msg'>
+                    <i className="fas fa-paper-plane"></i>
+                    GỬI
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </Layout>
--- src/views/services/trainingService/trainingService.js
+++ src/views/services/trainingService/trainingService.js
@@ -1,14 +1,154 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Layout from '../../layout/layout';
+import './trainingService.scss';
+import icBanner from '../../../assets/img/ic-banner-training.jpg';
 
+const listCourseHot = [
+  {
+    title: "Phân tích kỹ thuật cho người mới bắt đầu",
+    src: 'abc.com.vn'
+  },
+  {
+    title: "Phân tích kỹ thuật cho người mới bắt đầu",
+    src: 'abc.com.vn'
+  },
+  {
+    title: "Phân tích kỹ thuật cho người mới bắt đầu",
+    src: 'abc.com.vn'
+  },
+  {
+    title: "Phân tích kỹ thuật cho người mới bắt đầu",
+    src: 'abc.com.vn'
+  },
+  {
+    title: "Phân tích kỹ thuật cho người mới bắt đầu",
+    src: 'abc.com.vn'
+  },
+  {
+    title: "Phân tích kỹ thuật cho người mới bắt đầu",
+    src: 'abc.com.vn'
+  },
+  {
+    title: "Phân tích kỹ thuật cho người mới bắt đầu",
+    src: 'abc.com.vn'
+  },
+  {
+    title: "Phân tích kỹ thuật cho người mới bắt đầu",
+    src: 'abc.com.vn'
+  },
+]
+const listCourseOnline = [
+  {
+    title: "Khóa học hướng dẫn giao dịch mua bán Cổ phiếu - Lớp CKCS 108",
+    start: "24/6",
+    calendar: "18h30 - 21h thứ 2, 4, 6 hàng tuần"
+  },
+  {
+    title: "Khóa học hướng dẫn giao dịch mua bán Cổ phiếu - Lớp CKCS 109",
+    start: "24/6",
+    calendar: "18h30 - 21h thứ 2, 4, 6 hàng tuần"
+  },
+  {
+    title: "Hướng dẫn giao dịch mua bán ETF - ETF  104",
+    start: "24/6",
+    calendar: "18h30 - 21h thứ 2, 4, 6 hàng tuần"
+  },
+  {
+    title: "Hướng dẫn giao dịch mua bán ETF - ETF  104",
+    start: "24/6",
+    calendar: "18h30 - 21h thứ 2, 4, 6 hàng tuần"
+  }
+]
+const listCourseOffline = [
+  {
+    title: "Khóa học hướng dẫn giao dịch mua bán Cổ phiếu - Lớp CKCS 108 (Offline)",
+    start: "24/6",
+    calendar: "18h30 - 21h thứ 2, 4, 6 hàng tuần"
+  },
+  {
+    title: "Khóa học hướng dẫn giao dịch mua bán Cổ phiếu - Lớp CKCS 108 (Offline)",
+    start: "24/6",
+    calendar: "18h30 - 21h thứ 2, 4, 6 hàng tuần"
+  },
+  {
+    title: "Hướng dẫn giao dịch mua bán ETF - ETF  104 (Offline)",
+    start: "24/6",
+    calendar: "18h30 - 21h thứ 2, 4, 6 hàng tuần"
+  },
+  {
+    title: "Hướng dẫn giao dịch mua bán ETF - ETF  104 (Offline)",
+    start: "24/6",
+    calendar: "18h30 - 21h thứ 2, 4, 6 hàng tuần"
+  }
+]
 class TrainingService extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedCourse: "Online"
+    }
+  }
+
+  onChangeSelectCourse = (type) => {
+    this.setState({
+      selectedCourse: type
+    })
+  }
+
   render() {
     return(
       <Layout title="">
-        <div>
-          <h1>Dịch vụ đào tạo</h1>
-          
+        <div className='training-service-page'>
+          <div className='banner'>
+            <img alt='img' src={ icBanner }/>
+          </div>
+          <div className='course-box'>
+            <div className='title'>DANH SÁCH KHÓA HỌC</div>
+            <hr />
+            <div className='list-course-box'>
+              <div className='select-course-type'>
+                <div className='type-course active' onClick={() => this.onChangeSelectCourse('Online')} >Khóa học Online</div>
+                <div className='type-course' onClick={() => this.onChangeSelectCourse('Offline')} >Khóa học Offline</div>
+              </div>
+              {
+                this.state.selectedCourse === "Online" ? <div className='list-course'>
+                  {
+                    listCourseOnline.map((item, index) => {
+                      return <div key={index} className='course-item'>
+                        <div className='start-time'><span>KHAI GIẢNG</span>{item.start}</div>
+                        <div className='title'>{item.title}</div>
+                        <div className='calendar'>{item.calendar}</div>
+                      </div>
+                    })
+                  }
+                </div> : <div className='list-course'>
+                  {
+                    listCourseOffline.map((item, index) => {
+                      return <div key={index} className='course-item'>
+                        <div className='start-time'><span>KHAI GIẢNG</span>{item.start}</div>
+                        <div className='title'>{item.title}</div>
+                        <div className='calendar'>{item.calendar}</div>
+                      </div>
+                    })
+                  }
+                </div>
+              }
+              
+            </div>
+            <div className='hot-course'>
+              <div className='title'>KHÓA HỌC HOT</div>
+              {
+                listCourseHot.map((item, index) => {
+                  return <div className='row-course' key={index}>
+                    <div className='course-index'>{index}</div>
+                    <div className='course-name'>{item.title}</div>
+                  </div>
+                })
+              }
+            </div>
+          </div>
         </div>
       </Layout>
     )
