b7b6a960b94f7b18a11a2a76519eded57e4075ff
Nguyen Canh Duc Due <Due.NguyenCanhDuc@thanglongsc.com.vn>
2019-06-19 09:18:01 +0700

    Updates

---
+++ precache-manifest.3c9be6c27baa597bc1f43f35087c1dfe.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "699400c4b47baeb1d38f",
+    "url": "/static/css/main.d2a96b25.chunk.css"
+  },
+  {
+    "revision": "699400c4b47baeb1d38f",
+    "url": "/static/js/main.519beed3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "258945ccfef14492f449",
+    "url": "/static/css/2.19ce8650.chunk.css"
+  },
+  {
+    "revision": "258945ccfef14492f449",
+    "url": "/static/js/2.621645ed.chunk.js"
+  },
+  {
+    "revision": "72fff3bcf074c898fae7d936a5082d2a",
+    "url": "/static/media/bg_SignIn.72fff3bc.jpg"
+  },
+  {
+    "revision": "015b4fcf38e45ff017e01054d9c15f9b",
+    "url": "/static/media/ic_no_img2.015b4fcf.png"
+  },
+  {
+    "revision": "43d3a4487ebc182a0800ce4fbdfb71d3",
+    "url": "/static/media/banner1.43d3a448.jpg"
+  },
+  {
+    "revision": "43d3a4487ebc182a0800ce4fbdfb71d3",
+    "url": "/static/media/banner2.43d3a448.jpg"
+  },
+  {
+    "revision": "43d3a4487ebc182a0800ce4fbdfb71d3",
+    "url": "/static/media/banner3.43d3a448.jpg"
+  },
+  {
+    "revision": "66809499e63ed7184fb2b26b44ad855a",
+    "url": "/static/media/bg_huong_dan_giao_dich.66809499.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "54b0a99349164fc79b5ac267b818ac6e",
+    "url": "/static/media/slide_04.54b0a993.jpg"
+  },
+  {
+    "revision": "6643e879ed64a0fe81a27ca2f7a48d46",
+    "url": "/static/media/slide_03.6643e879.jpg"
+  },
+  {
+    "revision": "715b48e382ae12ad7b3850084e44452f",
+    "url": "/static/media/slide_02.715b48e3.jpg"
+  },
+  {
+    "revision": "569c696d1ddfee393676d973bc4f82fd",
+    "url": "/static/media/slide_01.569c696d.jpg"
+  },
+  {
+    "revision": "a0d8e8f7f24960765dff6f349f6aa56c",
+    "url": "/static/media/bg_news.a0d8e8f7.jpg"
+  },
+  {
+    "revision": "3dfe797cbfd6b73419a61fceb983a8f7",
+    "url": "/static/media/ic-banner-consulting.3dfe797c.png"
+  },
+  {
+    "revision": "74821581e77971b98089356b0e1fd6eb",
+    "url": "/static/media/expert-bg.74821581.png"
+  },
+  {
+    "revision": "11cda1bbf00907d8a2a2910a3d422a4b",
+    "url": "/static/media/bg_open_account.11cda1bb.jpg"
+  },
+  {
+    "revision": "35635a36ed7f285f8e4445ed28400957",
+    "url": "/static/media/ic-banner-training.35635a36.png"
+  },
+  {
+    "revision": "308ae77082f3f02c51bfd2ae63e1d0c9",
+    "url": "/static/media/banner-report.308ae770.png"
+  },
+  {
+    "revision": "a03cb67c99242a8532352c19025bddf6",
+    "url": "/static/media/banner-aboutus.a03cb67c.png"
+  },
+  {
+    "revision": "308ae77082f3f02c51bfd2ae63e1d0c9",
+    "url": "/static/media/banner-kienthuc.308ae770.png"
+  },
+  {
+    "revision": "ed005799a8c71fb3e4252da115c0200f",
+    "url": "/static/media/banner-tuvanCKPS.ed005799.png"
+  },
+  {
+    "revision": "720b73f0f7b7f2447192b9b9c54bd7c3",
+    "url": "/static/media/banner_ctAccount.720b73f0.jpg"
+  },
+  {
+    "revision": "21c5ca59e867ea5b8c109328e3d074fe",
+    "url": "/static/media/BANNER_SEARCH.21c5ca59.jpg"
+  },
+  {
+    "revision": "5a798cdadc7cd321e3f72425b70bface",
+    "url": "/static/media/OpenSans-Regular.5a798cda.ttf"
+  },
+  {
+    "revision": "0062c34665a3fc0f2278cd4e955702ec",
+    "url": "/static/media/OpenSans-Bold.0062c346.ttf"
+  },
+  {
+    "revision": "8634364c71a378a6130fe7931fd7d42b",
+    "url": "/index.html"
+  }
+];
